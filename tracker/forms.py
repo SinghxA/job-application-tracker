916ebc3d8ee45bc7f5bbaf7bb8074aa37715c406
Anjali15791 <anjalisingh4846@gmail.com>
2026-09-19 18:40:10 +0530

    Add date picker to application form

--- tracker/forms.py
+++ tracker/forms.py
@@ -5,3 +5,8 @@ class JobApplicationForm(forms.ModelForm):
     class Meta:
         model = JobApplication
         fields = ['company_name', 'role', 'date_applied', 'status']
+        widgets = {
+            'date_applied': forms.DateInput(
+                attrs={'type': 'date'}
+            ),
+        }
